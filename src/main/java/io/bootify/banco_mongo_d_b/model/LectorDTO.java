f94b77cc3228bdb26603226a34e669e934e70ee9
Juank003 <jortealv@myuax.com>
2023-12-03 22:15:24 +0100

    Funciona

---
+++ src/main/java/io/bootify/banco_mongo_d_b/model/LectorDTO.java
@@ -0,0 +1,18 @@
+package io.bootify.banco_mongo_d_b.model;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class LectorDTO {
+
+    private Long id;
+
+    @NotNull
+    @Size(max = 255)
+    private String apellido;
+
+}
